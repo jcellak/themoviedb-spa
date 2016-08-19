3c9d42d07897bb41590dbd4bf0e68920c4631909
Jeffrey Ellak <jellak@idtech.com>
2016-08-19 16:53:55 -0700

    First draft of completed project (barring any tweaks)

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# themoviedb-spa
+# MovieDB SPA
+
+An AngularJS powered app, bundled with bootstrap CSS and the [TMDb](https://themoviedb.org) API to demonstrate rapid Web front-end development.
+
+Getting Started
+-
+
+Download the .zip file [here](https://github.com/jcellak/themoviedb-spa/archive/master.zip).
+
+Open index.html in a browser.
+
+Local Dev Environment vs. Hosted Prod Environment
+-
+
+I opted to use public CDNs for serving up CSS and JS libraries for this project. (Specifically, AngularJS v1 and Bootstrap)
+
+For a production environment I would use build tools that allow me to append or build specific versions, as well as minifying/combining all CSS and JS (non-library) files.
+
+Given the scope of the project, I opted to use regular CSS, but for larger projects I would use LESS and the aforementioned build tools to keep things more organized.
+
+There are also a few key areas that could be separated out into AngularJS Element directives, but since everything here is local; it wouldn't work due to local origin restrictions.
+
+The hard-coded values (for the API) would also be better served if the config JSON were cached once every few days on a hosted server, and the API key kept private via server-side only requests.
+
+License
+-
+
+This project is meant for educational purposes only!
---
+++ src/movie-browser.js
@@ -0,0 +1,148 @@
+// Wrapped in enclosure (separates from globals and overall good habit)
+(function() {
+    var app = angular.module('movie-browser', []),
+        api_key = '7ab870538b64f3fc26658d523e402ddd',
+        collection_id = '528',
+        stars_limit = 4,
+        api_moviedb_url = 'http://api.themoviedb.org/3/',
+        api_config_url = api_moviedb_url + 'configuration?api_key=' + api_key,
+        api_collection_url = api_moviedb_url + 'collection/' + collection_id + '?api_key=' + api_key,
+        api_movie_url = api_moviedb_url + 'movie/[id]?api_key=' + api_key + '&append_to_response=credits';
+
+    app.controller('DataController', ['$http', '$log', function($http, $log) {
+        var dataCtrl = this; // to avoid referencing $http as this during callbacks
+        this.config = {};
+        this.collection = {};
+        this.currentMovie = 0;
+        this.currentCast = 0;
+
+        $http.get(api_config_url).then(configSuccessCallback, errorCallback);
+        $http.get(api_collection_url).then(collectionSuccessCallback, errorCallback);
+
+        /**
+         * Success callback for TMDb API configuration values. Saves to session to cut down on requests.
+         * NOTE: Ideal solution would be to save config values from TMDb server-side and refresh once every few days.
+         * @param {{data: string|Object, status: number, headers: function, config: Object, statusText: string}} response
+         */
+        function configSuccessCallback(response) {
+            dataCtrl.config = response.data;
+        }
+
+        /**
+         * Success callback for a movie collection. Appends additional data to each movie in the collection through
+         * additional separate ajax requests.
+         * @param {{data: string|Object, status: number, headers: function, config: Object, statusText: string}} response
+         */
+        function collectionSuccessCallback(response) {
+            dataCtrl.collection = response.data;
+            dataCtrl.collection.parts.forEach(function(element, index, array) {
+                $http.get(api_movie_url.replace(/\[id]/g, element.id)).then(function(response) {
+                    dataCtrl.collection.parts[index] = response.data; // replace minimal movie data Objects with the full dataset
+                }, errorCallback);
+            });
+        }
+
+        /**
+         * Generic error callback for any failed ajax requests.
+         * @param {{data: string|Object, status: number, headers: function, config: Object, statusText: string}} response
+         */
+        function errorCallback(response) {
+            $log.log(response);
+        }
+
+        /**
+         * @param currentMovie Array index of the currently selected cast movie.
+         */
+        this.setCurrentMovie = function(currentMovie) {
+            dataCtrl.currentMovie = currentMovie;
+            dataCtrl.setCurrentCast(0); // reset selected cast member on new movie
+        };
+
+        /**
+         * @param index
+         * @returns {boolean}
+         */
+        this.isCurrentMovie = function(index) {
+            return dataCtrl.currentMovie === index;
+        };
+
+        /**
+         * @param currentCast Array index of the currently selected cast member.
+         */
+        this.setCurrentCast = function(currentCast) {
+            dataCtrl.currentCast = currentCast;
+        };
+
+        /**
+         * @param index
+         * @returns {boolean}
+         */
+        this.isCurrentCast = function(index) {
+            return dataCtrl.currentCast === index;
+        };
+
+        /**
+         * @param path
+         * @param imageSize
+         * @returns {string}
+         */
+        this.imageUrl = function(path, imageSize) {
+            return this.config.images.base_url + imageSize + path;
+        };
+    }]);
+
+    app.filter('movieDirectorName', function() {
+        return function(movie) {
+            if (!movie.credits) { // two-way data binding not set yet
+                return '';
+            }
+
+            for (var i = 0; i < movie.credits.crew.length; i++) {
+                if (movie.credits.crew[i].job.toLocaleLowerCase() == "director") {
+                    return movie.credits.crew[i].name;
+                }
+            }
+        };
+    });
+
+    app.filter('movieWriterNames', function() {
+        return function(movie) {
+            var writers = [];
+            if (!movie.credits) { // two-way data binding not set yet
+                return '';
+            }
+
+            for (var i = 0; i < movie.credits.crew.length; i++) {
+                if (movie.credits.crew[i].department.toLocaleLowerCase() == "writing") {
+                    writers.push(movie.credits.crew[i].name);
+                }
+            }
+
+            return writers.join(', ');
+        };
+    });
+
+    app.filter('movieStarNames', function() {
+        return function(movie) {
+            var stars = [];
+            if (!movie.credits) { // two-way data binding not set yet
+                return '';
+            }
+
+            for (var i = 0; i < movie.credits.cast.length && i < stars_limit; i++) {
+                stars.push(movie.credits.cast[i].name);
+            }
+
+            return stars.join(', ');
+        };
+    });
+
+    app.directive('backImg', function() {
+        return function(scope, element, attrs) {
+            var url = attrs.backImg;
+            element.css({
+                'background-image': 'url(' + url + ')'
+            });
+        };
+    });
+})();
